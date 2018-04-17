182dbc3cc203d0a9c4b8d3202dbf86e5bd722dba
zjwzt1123 <33337293+zuelzt@users.noreply.github.com>
2018-04-17 22:24:35 +0800

    Update jingdong.py

--- jingdong.py
+++ jingdong.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Apr 16 21:28:33 2018
+Created on Tue Apr 17 21:29:55 2018
 
 @author: Rorschach
 @mail: 188581221@qq.com
@@ -13,10 +13,16 @@ import re
 import requests as rq
 import time
 import pymysql as sql
+import datetime
 
+# import last time
+with open('/Users/zt/Desktop/time.txt','r') as zjw:
+    last_time = zjw.read()
+
+# get id
 begin = time.time()
 all_id = []
-pages = range(1,6)
+pages = range(1,8)
 for page in pages:
     url = 'https://mideash.jd.com/view_search-402471-1000001281-1000001281-0-2-0-0-1-'+ str(page) + '-60.html?isGlobalSearch=0&other='
     browser = webdriver.Chrome()
@@ -42,11 +48,11 @@ item_id = list(set(all_id))
 # =============================================================================
 
 # connect mysql
-con = sql.connect(host='localhost', user='xxxx',passwd='xxxxx',db='jingdong',charset='utf8')
+con = sql.connect(host='localhost', user='root',passwd='',db='jingdong',charset='utf8')
 
 # begin
 for itemId in item_id:
-    total_pages = 1000
+    total_pages = 100
     count = 0
     comments = []
     times = []
@@ -66,9 +72,19 @@ for itemId in item_id:
                 names.append(item_name[num][1:-1])
                 count += 1
                 print('get No. {}'.format(count))
+        time.sleep(0.05)
+        # select
+        for i in range(len(times)):
+            if times[i] < last_time:
+                cut = i
+                break
+        comments = comments[:cut]
+        times = times[:cut]
+        names = names[:cut]
+                
     # mysql
     cursor = con.cursor()
-    item_table = 'create table table' + str(itemId) + '(id int not null auto_increment primary key, name varchar(1000),time datetime,comments varchar(10000000))' 
+    item_table = 'create table if not exists table' + str(itemId) + '(id int not null auto_increment primary key, name varchar(1000),time datetime,comments varchar(10000000))' 
     cursor.execute(item_table)
     cursor.close()
     for i in range(len(comments)):
@@ -86,3 +102,9 @@ for itemId in item_id:
 con.close()
 end = time.time()
 print('Total {0:.1f} min !'.format((end-begin)/60))
+
+# time
+end_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+with open('/Users/zt/Desktop/time.txt','w') as zt:
+    zt.write(end_time)
+    
