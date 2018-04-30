7effc60220eeb17766820aa5e81a8b893900eb4f
zjwzt1123 <33337293+zuelzt@users.noreply.github.com>
2018-04-30 09:06:30 +0800

    Update del.py

--- del.py
+++ del.py
@@ -14,29 +14,31 @@ from sqlalchemy import create_engine
 import time 
 
 begin = time.time()
-brands = ['del']#, 'huawei', 'iphone']
+brands = ['xiaomi']#, 'huawei', 'iphone']
 for brand in brands:
     # connect mysql
     db_info = {'user': 'root',
                'password': '',
                'host': 'localhost',
                'database': brand}
-    engine = create_engine('mysql+pymysql://%(user)s:%(password)s@%(host)s/%(database)s?charset=utf8' % db_info,encoding='utf-8')  
+    engine = create_engine('mysql+pymysql://%(user)s:@%(host)s/%(database)s?charset=utf8' % db_info,encoding='utf-8')  
     con = sql.connect(host='localhost', user='root', passwd='', db=brand, charset='utf8')
     find_name = 'select table_name from information_schema.tables where table_schema=\'' + brand + '\' and table_type=\'base table\';'
     table_names = list(pd.read_sql(find_name, con)['table_name'])
+    con.close() 
     for name in table_names:
         rid = []
         ifo = 'select * from ' + name
+        con = sql.connect(host='localhost', user='root', passwd='', db=brand, charset='utf8')
         data = pd.read_sql(ifo, con)
+        con.close() 
         ori = len(data)
         for i in range(ori):
             if i != ori-1:
                 if str(data[i:i+1]['comments'].values[0]) == str(data[i+1:i+2]['comments'].values[0]):
                     rid.append(i+1)
         data = data.drop(rid)
-        data.to_sql(name, engine, if_exists='replace', index=False)  
-    con.close() 
+        pd.io.sql.to_sql(data, name, engine, if_exists='replace', index=False)  
 end = time.time()
 print('Total {0:.3f} min !'.format((end-begin)/60))  
   
