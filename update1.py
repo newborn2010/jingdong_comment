924d7ace8e515a1e061fdcdcbc7379b2f9ced7dc
zjwzt1123 <33337293+zuelzt@users.noreply.github.com>
2018-05-20 09:17:56 +0800

    Update update1.py

--- update1.py
+++ update1.py
@@ -16,6 +16,8 @@ import time
 import pymysql as sql
 import datetime
 
+rq.adapters.DEFAULT_RETRIES = 20 
+
 # import last time
 with open('/Users/zt/Desktop/time.txt','r') as lt:
     last_time = lt.read()
@@ -26,7 +28,7 @@ with open('/Users/zt/Desktop/time.txt','w') as nt:
     nt.write(end_time)
     
 # get id
-brands = ['xiaomi', 'huawei', 'iphone'] #'xiaomi', 'huawei', 'iphone','samsung', 'honor'
+brands = ['huawei', 'iphone'] #'xiaomi', 'huawei', 'iphone','samsung', 'honor'
 urls = {'xiaomi': ['https://mi.jd.com/view_search-442829-1000004123-1000004123-0-2-0-0-1-', '-60.html?keyword=%25E6%2589%258B%25E6%259C%25BA&isGlobalSearch=0&other=&isRedisstore=0'],
         'huawei': ['https://huawei.jd.com/view_search-466323-1000004259-1000004259-0-2-0-0-1-', '-60.html?keyword=%25E6%2589%258B%25E6%259C%25BA&isGlobalSearch=0&other=&isRedisstore=0'],
         'iphone': ['https://mall.jd.com/advance_search-394872-1000000127-1000000127-2-0-0-1-', '-60.html?keyword=%25E6%2589%258B%25E6%259C%25BA&other=&isRedisstore=0'],
@@ -56,7 +58,9 @@ for brand in brands:
     # begin
     number = 1
     update = 0
-    for itemId in item_id:
+    #if brand == 'xiaomi':
+       # item_id.remove(6813715)
+    for itemId in item_id:   
         total_pages = 200
         count = 0
         comments = []
@@ -74,7 +78,10 @@ for brand in brands:
         pic_num = [] 
         for i in range(total_pages):
             url = 'https://sclub.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv8571&productId=' + str(itemId) + '&score=0&sortType=6&page=' + str(i) + '&pageSize=10&isShadowSku=0&fold=1'
-            myweb = rq.get(url)
+            headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36', 
+                       'Connection': 'close'}
+            proxy = {'http': '33.33.33.10:8118'}
+            myweb = rq.get(url, headers=headers, proxies=proxy, timeout=30)
             comment_time_name = re.findall('\"topped\":(.*?)\,\"referenceName\"', myweb.text)
             item_name = re.findall('\"referenceName\":(\".*?\")\,\"referenceTime\"', myweb.text)
             score = re.findall('\"referenceType\":\"Product\"(.*?)\,\"status\"', myweb.text)
@@ -142,12 +149,12 @@ for brand in brands:
             item_table = 'create table if not exists table' + str(itemId) + '( name varchar(1000),item int, page int, time datetime,score int,day int,after_day int,good int, bad int,exp int,pic int, level varchar(30),comments varchar(10000000),after_comments varchar(10000000))' 
             cursor.execute(item_table)
             cursor.close()
-            cursor = con.cursor()
             for i in range(len(comments)):
+                cursor = con.cursor()
                 query = ('insert into table' + str(itemId) + '(name, item, page, time, score, day, after_day, good, bad, exp, pic, level, comments, after_comments) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)')
                 cursor.execute(query, (names[i], itemId, pages[i], times[i], scores[i], days[i], afterdays[i], goods[i], bads[i], exps[i], pic_num[i], levels[i], comments[i], aftercomments[i]))
-            con.commit()
-            cursor.close()         
+                con.commit()
+                cursor.close()         
     con.close()
     updates.append(update)
     time.sleep(300)
