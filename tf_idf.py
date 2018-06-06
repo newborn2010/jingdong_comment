1084e0d1deb4c5bfcbae9bb98911ebfa95a85877
zjwzt1123 <33337293+zuelzt@users.noreply.github.com>
2018-06-06 14:22:47 +0800

    Update tf_idf.py

--- tf_idf.py
+++ tf_idf.py
@@ -13,25 +13,23 @@ import time
 import datetime
 import numpy as np
 
-# 读取数据
-corpus = ['今天 天气 不错 但是 有 一点 热', 
-          '他 赢 了 篮球 总决赛', 
-          '糯米饭 很 好吃 闻 起来 很 香', 
-          '海南省 是 中国 的 旅游 胜地', 
-          '太阳系 是 银河系 的 一部分']
-
 def tf_idf(input_path, stop_word=None, show=10):
     '''
     输入已分词文本，以空格隔开，每一行为一个记录，输出 TF_IDF 主题。
     input_path: list
+    e.g.
+    corpus = ['今天 天气 不错 但是 有 一点 热', 
+          '他 赢 了 篮球 总决赛', 
+          '海南省 是 中国 的 旅游 胜地', 
+          '太阳系 是 银河系 的 一部分']
     '''
     begin = time.time()
-    print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    print('Start', datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     corpus = []
     for path in input_path:
         with open(path, 'r') as i:
             corpus.append(i.read().replace('\n', ' '))
-    print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    print('Loading done', datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     # 设置停用词
     if stop_word == None:
         with open('/Users/zt/Desktop/project/stop_words/stop_1.txt', 'r') as stop:
@@ -52,7 +50,7 @@ def tf_idf(input_path, stop_word=None, show=10):
     vectorizer.fit_transform(corpus)
     name = vectorizer.get_feature_names()
     array = vectorizer.fit_transform(corpus).toarray()
-    print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    print('Model done', datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     # 结果
     result = []
     for i in range(array.shape[0]):
@@ -60,28 +58,9 @@ def tf_idf(input_path, stop_word=None, show=10):
         sort = np.argsort(array[i])[::-1][:show]
         for j in sort:
             topic.append(name[j])
-        result.append((corpus[i], topic))
-    print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+        result.append(('corpus'+str(i+1), topic))
+    print('End', datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     end = time.time()
     print('Total {0:.3f} min !'.format((end-begin)/60))
-    return(print(result))
+    return(result)
         
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
