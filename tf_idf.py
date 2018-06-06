2efc7c7bc6ceeebda0be33d80b676e285f0fd621
zjwzt1123 <33337293+zuelzt@users.noreply.github.com>
2018-06-06 13:37:39 +0800

    Update tf_idf.py

--- tf_idf.py
+++ tf_idf.py
@@ -9,6 +9,8 @@ Created on Wed Jun  6 10:09:24 2018
 import warnings
 warnings.filterwarnings('ignore')
 from sklearn.feature_extraction.text import TfidfVectorizer
+import time
+import numpy as np
 
 # 读取数据
 corpus = ['今天 天气 不错 但是 有 一点 热', 
@@ -17,42 +19,41 @@ corpus = ['今天 天气 不错 但是 有 一点 热',
           '海南省 是 中国 的 旅游 胜地', 
           '太阳系 是 银河系 的 一部分']
 
-with open('/Users/zt/Desktop/cut.txt', 'r') as i:
-    corpus = []
-    for line in i.readlines():
-        corpus.append(line.rstrip('\n'))
-
-# 设置停用词
-with open('/Users/zt/Desktop/project/stop_words/stop_1.txt', 'r') as stop:
-    stop_words = []
-    for line in stop.readlines():
-        stop_words.append(line.rstrip('\n'))
-with open('/Users/zt/Desktop/project/stop_words/stop_2.txt', 'r') as stop:
-    for line in stop.readlines():
-        stop_words.append(line.rstrip('\n'))
-stop_words = list(set(stop_words))
-
-# tf_idf
-vectorizer = TfidfVectorizer(stop_words=stop_words, min_df=1)
-
-vectorizer.fit_transform(corpus)
-
-name = vectorizer.get_feature_names()
-array = vectorizer.fit_transform(corpus).toarray()
-
-# 结果
-result = []
-for i in range(array.shape[0]):
-    topic = []
-    for j in range(array.shape[1]):
-        if array[i][j] == array[i].max():
-            topic.append(name[j])    
-    result.append((corpus[i], topic))
-
-print(result)
+def tf_idf(input_path, stop_word=None, show=10):
+    '''
+    输入已分词文本，以空格隔开，每一行为一个记录，输出 TF_IDF 主题。
+    '''
+    begin = time.time()
+    with open('/Users/zt/Desktop/cut.txt', 'r') as i:
+        corpus = []
+        for line in i.readlines():
+            corpus.append(line.rstrip('\n'))
+    # 设置停用词
+    with open('/Users/zt/Desktop/project/stop_words/stop_1.txt', 'r') as stop:
+        stop_words = []
+        for line in stop.readlines():
+            stop_words.append(line.rstrip('\n'))
+    with open('/Users/zt/Desktop/project/stop_words/stop_2.txt', 'r') as stop:
+        for line in stop.readlines():
+            stop_words.append(line.rstrip('\n'))
+    stop_words = list(set(stop_words))
+    # tf_idf
+    vectorizer = TfidfVectorizer(stop_words=stop_words, min_df=1)
+    vectorizer.fit_transform(corpus)
+    name = vectorizer.get_feature_names()
+    array = vectorizer.fit_transform(corpus).toarray()
+    # 结果
+    result = []
+    for i in range(array.shape[0]):
+        topic = []
+        sort = np.argsort(array[i])[::-1][:10]
+        for j in sort:
+            topic.append(name[j])
+        result.append((corpus[i], topic))
+    end = time.time()
+    print('Total {0:.3f} min !'.format((end-begin)/60))
+    return(result[:show])
         
-
-
 
 
 
