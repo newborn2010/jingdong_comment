51321557e3e5c7fd388b0b2267a0be34c8c74138
zjwzt1123 <33337293+zuelzt@users.noreply.github.com>
2018-06-06 14:37:05 +0800

    Update tf_idf.py

--- tf_idf.py
+++ tf_idf.py
@@ -18,18 +18,19 @@ def tf_idf(input_path, stop_word=None, show=10):
     输入已分词文本，以空格隔开，每一行为一个记录，输出 TF_IDF 主题。
     input_path: list
     e.g.
+    input_path = ['cut1.txt', 'cut2.txt', 'cut3.txt']
     corpus = ['今天 天气 不错 但是 有 一点 热', 
           '他 赢 了 篮球 总决赛', 
           '海南省 是 中国 的 旅游 胜地', 
           '太阳系 是 银河系 的 一部分']
     '''
     begin = time.time()
-    print('Start', datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    print('Start :', datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     corpus = []
     for path in input_path:
         with open(path, 'r') as i:
             corpus.append(i.read().replace('\n', ' '))
-    print('Loading done', datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    print('Loading done :', datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     # 设置停用词
     if stop_word == None:
         with open('/Users/zt/Desktop/project/stop_words/stop_1.txt', 'r') as stop:
@@ -50,7 +51,7 @@ def tf_idf(input_path, stop_word=None, show=10):
     vectorizer.fit_transform(corpus)
     name = vectorizer.get_feature_names()
     array = vectorizer.fit_transform(corpus).toarray()
-    print('Model done', datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    print('Model done :', datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     # 结果
     result = []
     for i in range(array.shape[0]):
@@ -59,7 +60,7 @@ def tf_idf(input_path, stop_word=None, show=10):
         for j in sort:
             topic.append(name[j])
         result.append(('corpus'+str(i+1), topic))
-    print('End', datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    print('End :', datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     end = time.time()
     print('Total {0:.3f} min !'.format((end-begin)/60))
     return(result)
